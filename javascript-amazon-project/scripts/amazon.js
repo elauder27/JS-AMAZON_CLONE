0c0c802fd8ab07356507437d0729a05540bd725b
Esther <elauder549@gmail.com>
2026-03-03 11:27:45 +0100

    first git initialization and commit

---
+++ javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,119 @@
+/*
+//data structure to hold product information
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+  },
+
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: " Intermediate Size Basketball",
+    rating: {
+      stars: 4.0,
+      count: 127,
+    },
+    priceCents: 2095,
+  },
+
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: " Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+  },
+];
+*/
+//modules(put all imports at the top of the file, we need to use live server to use modules )
+/** benefits of modules
+ * 1. better organization of code
+ * 2. avoid polluting global namespace
+ * 3. easier to maintain and reuse code
+ * we dont have to worry about the order of our files
+ */
+
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
+let productsHTML = "";
+
+products.forEach((product) => {
+  productsHTML += `
+        <div class="product-container">
+                <div class="product-image-container">
+                    <img class="product-image"
+                    src="${product.image}">
+                </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                    ${product.name}
+                </div>
+
+                <div class="product-rating-container">
+                    <img class="product-rating-stars"
+                    src="images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png">
+                    <div class="product-rating-count link-primary">
+                    ${product.rating.count}
+                    </div>
+                </div>
+
+                <div class="product-price">
+                   $${(product.priceCents / 100).toFixed(2)}
+                </div>
+
+                <div class="product-quantity-container">
+                    <select>
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    </select>
+                </div>
+
+                <div class="product-spacer"></div>
+
+                <div class="added-to-cart">
+                    <img src="images/icons/checkmark.png">
+                    Added
+                </div>
+
+                <button class="add-to-cart-button button-primary js-add-to-cart-button"
+                data-product-id="${product.id}">
+                    Add to Cart
+                </button>   
+                </div>
+        `;
+});
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
+  });
+});
---
+++ javascript-amazon-project/scripts/checkout.js
@@ -0,0 +1,4 @@
+import { renderOrderSummary } from "./checkout/order-summary.js";
+renderOrderSummary();
+import { renderPaymentSummary } from "./checkout/payment-summary.js";
+renderPaymentSummary();
