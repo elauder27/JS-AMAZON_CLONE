906c5d04bcc1b1b6deb2cad17fdb399dbe62bdc5
Esther <elauder549@gmail.com>
2026-04-10 20:56:50 +0100

    learnt and applied promises and changed from using callback to using promises

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -41,11 +41,14 @@ const products = [
  */
 
 import { cart, addToCart } from "../data/cart.js";
-import { products } from "../data/products.js";
-let productsHTML = "";
+import { products, loadProducts } from "../data/products.js";
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
+function renderProductsGrid() {
+  let productsHTML = "";
+
+  products.forEach((product) => {
+    productsHTML += `
         <div class="product-container">
                 <div class="product-image-container">
                     <img class="product-image"
@@ -99,22 +102,23 @@ products.forEach((product) => {
                 </button>   
                 </div>
         `;
-});
-
-function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
+
+  document.querySelector(".js-products-grid").innerHTML = productsHTML;
+  document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
 }
-
-document.querySelector(".js-products-grid").innerHTML = productsHTML;
-document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-    addToCart(productId);
-    updateCartQuantity();
-  });
-});
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,7 +1,50 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
-renderOrderSummary();
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
-renderPaymentSummary();
+
 /* import "../data/cart-class.js";
  */
-import "../data/backend-practice.js";
+//promises helps keep our code flat to prevent more nesting
+import { loadCart } from "../data/cart.js";
+import { loadProducts } from "../data/products.js";
+
+Promise.all([
+  newPromise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  newPromise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+//promises are a way to wait for asynchronous code to run
+/* new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+})
+  .then(() => {
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  }); */
+
+/* loadProducts(() => {
+  loadCart(() => {
+   renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+ */
